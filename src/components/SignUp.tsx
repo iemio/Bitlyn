ffdcea2f1ec9a158f25dca318f5bab7a43b11bce
iemio <deeplearner4u@gmail.com>
2025-06-28 21:23:41 +0530

    feat: added amine

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -17,7 +17,7 @@ const SignUp = () => {
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
-                Already a Bitizen?{" "}
+                Already a Bitlyst?{" "}
                 <Link
                     href="/sign-in"
                     className="hover:text-brand text-sm underline underline-offset-4"
